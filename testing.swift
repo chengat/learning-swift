e153ed763ce8ce9ca95b12a94dd026b89ce2fab8
Nikhil Nambiar Chengat <nikhilnambiar1212@gmail.com>
2025-12-04 14:00:41 -0500

    syntax for variables, constants, datatypes, printing variables

--- testing.swift
+++ testing.swift
@@ -6,3 +6,15 @@
 //
 
 print("Hello World") // print statement
+
+var age = 23 // for variables, declaring like this will allow compiler to infer data type
+let name = "Nikhil" // for constants, cant be changed, similar to adding final to variables in java
+
+// Common data types: String, Bool, Int, Double
+var lastName: String = "Chengat" // to specify the datatype for variable
+var isStudent: Bool = false
+var height: Int = 188
+var weight: Double = 205.6
+
+// printing variables: \(varName) aka string interpolation
+print("Hello \(name) \(lastName)! You are \(age) and your height is \(height) cm and your weight is \(weight) lbs." )
