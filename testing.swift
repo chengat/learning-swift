ae248e5d64c3d9ecaf5a846d7d22286f42439ac6
Nikhil Nambiar Chengat <nikhilnambiar1212@gmail.com>
2025-12-10 18:56:48 -0500

    swtich statements

--- testing.swift
+++ testing.swift
@@ -225,5 +225,17 @@ if scoreB >= 90 {
 } else {
     print("C or lower")
 }
- 
+
+// switch statements
+let grade = 82
+switch grade {
+case 90...100:
+    print("A")
+case 80..<90:
+    print("B")
+case 70..<80:
+    print("C")
+default:
+    print("Below C")
+}
  
